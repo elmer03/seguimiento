3d8d68378c354dd37a7a90944787351332f2c509
maylee2016 <mayperez2010@gmail.com>
2016-12-17 23:57:05 -0700

    vista formulario seguimiento

--- views/rptSegumientoDocente.view.php
+++ views/rptSegumientoDocente.view.php
@@ -12,17 +12,16 @@
             <button type="button" onclick="$.print('#contenido');" class="btn btn-primary">imprimir</button>
             <div id="contenido">
                 <div class="row">
-                    <div class="col-md-3 text-left"><h4>UNIVERSIDAD MAYOR DE SAN SIMON</h4>
+                    <div class="col-md-3 text-left">
+                        <h4>UNIVERSIDAD MAYOR DE SAN SIMÓN</h4>
                         <h4>VICERECTORADO</h4>
                     </div>
                     <div class="col-md-6 text-center">
-                        <h3>SEGUIMIENTO ACADEMICO DOCENTE &nbsp;I-2015</h3>
+                        <h3>SEGUIMIENTO ACADEMICO DOCENTE </h3>  <!--&nbsp;I-2015-->
                     </div>
                     <div class="col-md-3 text-right">
-                        <h4>
-                            D.P.A.- D.I.S.U.
-                        </h4>
-                        <h6>Periodo Académico I-2015</h6>
+                        <h4> D.P.A.- D.I.S.U.</h4>
+                        <h6>Periodo Académico</h6>
                     </div>
                 </div>
                 <div class="row">
@@ -30,45 +29,51 @@
                         <tbody>
                         <tr>
                             <td rowspan="2" width="10%"><h6>DOCENTE</h6></td>
-                            <td>Ariel Oscar</td>
-                            <td>angoloa</td>
-                            <td>mendoza</td>
-                            <td>apellido esposo</td>
-                            <td rowspan="2"><h6>CI</h6></td>
-                            <td>64646344</td>
+                            <td class="test-center"><h6>Ariel Oscar</h6></td>
+                            <td class="test-center"><h6> Angoloa </h6></td>
+                            <td class="test-center"><h6> Mendoza </h6></td>
+                            <td class="test-center"><h6> Apellido esposo </h6></td>
+                            <td rowspan="2"><h6><b>CI</b></h6></td>
+                            <td><h6>64646344</h6></td>
                         </tr>
                         <tr>
-                            <td class="text-center"><h6>NOMBRE(S)</h6></td>
-                            <td class="text-center"><h6>AP. PATERNO</h6></td>
-                            <td class="text-center"><h6>AP. MATERNO</h6></td>
-                            <td class="text-center"><h6>AP. ESPOSO</h6></td>
+                            <td class="text-center"><h6><b>NOMBRE(S</b>)</h6></td>
+                            <td class="text-center"><h6><b>AP. PATERNO</b></h6></td>
+                            <td class="text-center"><h6><b>AP. MATERNO</b></h6></td>
+                            <td class="text-center"><h6><b>AP. ESPOSO</b></h6></td>
                         </tr>
                         </tbody>
                     </table>
                 </div>
                 <div class="row">
-                    <table class="table table-bordered">
+                    <table>
                         <thead>
                         <tr>
                             <th></th>
-                            <th>FACULTAD</th>
-                            <th>CARRERA</th>
-                            <th>DEPARTAMENTO</th>
-                            <th>MATERIAS</th>
-                            <th>SIGLA</th>
-                            <th>H.Teor.</th>
-                            <th>H.Prac.</th>
-                            <th>Anual</th>
-                            <th>Semestral</th>
+                            <th width="20%">FACULTAD</th>
+                            <th></th>
+                            <th width="20%">CARRERA</th>
+                            <th></th>
+                            <th width="20%">DEPARTAMENTO</th>
+                            <th></th>
+                            <th width="20%">MATERIAS</th>
+                            <th width="20%">SIGLA</th>
+                            <th width="10%">H.Teor.</th>
+                            <th width="10%">H.Prac.</th>
+                            <th width="5%">Anual</th>
+                            <th width="5%">Semestral</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
                             <td>1.-</td>
-                            <td>CIENCIAS Y TECNOLOGIA</td>
-                            <td>INF-SIS</td>
-                            <td>INFORMATICA-SISTEMAS</td>
-                            <td>MATEMATICA COMPUTACIONAL II</td>
+                            <td width="20%"><h6>CIENCIAS Y TECNOLOGIA</h6></td>
+                            <td>1.-</td>
+                            <td width="20%"><h6>INF-SIS</h6></td>
+                            <td>1.-</td>
+                            <td width="20%"><h6>INFORMATICA-SISTEMAS</h6></td>
+                            <td>1.-</td>
+                            <td width="20%"><h6>MATEMATICA COMPUTACIONAL II</h6></td>
                             <td>2008214</td>
                             <td>0</td>
                             <td>0</td>
@@ -78,8 +83,11 @@
                         <tr>
                             <td>2.-</td>
                             <td>CIENCIAS Y TECNOLOGIA</td>
+                            <td>2.-</td>
                             <td>INF-SIS</td>
+                            <td>2.-</td>
                             <td>INFORMATICA-SISTEMAS</td>
+                            <td>2.-</td>
                             <td>BASE DE DATOS DISTRIBUIDAS</td>
                             <td>155555</td>
                             <td>0</td>
@@ -87,15 +95,157 @@
                             <td><input type="checkbox"></td>
                             <td><input type="checkbox" checked></td>
                         </tr>
+                        <tr>
+                            <td>3.-</td>
+                            <td></td>
+                            <td>3.-</td>
+                            <td></td>
+                            <td>3.-</td>
+                            <td></td>
+                            <td>3.-</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><input type="checkbox"></td>
+                            <td><input type="checkbox"></td>
+                        </tr>
+                        <tr>
+                            <td>4.-</td>
+                            <td></td>
+                            <td>4.-</td>
+                            <td></td>
+                            <td>4.-</td>
+                            <td></td>
+                            <td>4.-</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><input type="checkbox"></td>
+                            <td><input type="checkbox"></td>
+                        </tr>
+                        <tr>
+                            <td>5.-</td>
+                            <td></td>
+                            <td>5.-</td>
+                            <td></td>
+                            <td>5.-</td>
+                            <td></td>
+                            <td>5.-</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><input type="checkbox"></td>
+                            <td><input type="checkbox"></td>
+                        </tr>
+                        <tr>
+                            <td>6.-</td>
+                            <td></td>
+                            <td>6.-</td>
+                            <td></td>
+                            <td>6.-</td>
+                            <td></td>
+                            <td>6.-</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><input type="checkbox"></td>
+                            <td><input type="checkbox"></td>
+                        </tr>
+                        <tr>
+                            <td>7.-</td>
+                            <td></td>
+                            <td>7.-</td>
+                            <td></td>
+                            <td>7.-</td>
+                            <td></td>
+                            <td>7.-</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><input type="checkbox"></td>
+                            <td><input type="checkbox"></td>
+                        </tr>
+                        <tr>
+                            <td>8.-</td>
+                            <td></td>
+                            <td>8.-</td>
+                            <td></td>
+                            <td>8.-</td>
+                            <td></td>
+                            <td>8.-</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><input type="checkbox"></td>
+                            <td><input type="checkbox"></td>
+                        </tr>
                         </tbody>
 
                     </table>
 
-                    <td rowspan="2" width="10%"><h6>FACULTAD</h6></td>
-
 
                 </div>
 
+                <div class="row">
+                    <table>
+                        <thead>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Hrs. Teoria</td>
+                            <td>Hrs. Práctica</td>
+                            <td>Hrs. Producción</td>
+                            <td>R.C.F.Nº</td>
+                            <td>RESUMEN</td>
+                        </tr>
+                        <tr>
+                            <td>Hrs. de Investigación</td>
+                            <td>R.C.F.Nº</td>
+                            <td>Hrs. de Servicio Acad.</td>
+                            <td>R.C.F.Nº</td>
+                            <td>TOTAL HORAS/SEMANA TRABAJADAS</td>
+                        </tr>
+                        <tr>
+                            <td>Hrs. Extensión</td>
+                            <td>R.C.F.Nº</td>
+                            <td>Hrs. de Producc. Acad.</td>
+                            <td>R.C.F.Nº</td>
+                            <td>TOTAL HORAS/MES TRABAJADAS</td>
+                        </tr>
+                        <tr>
+                            <td>Hrs. Servicio</td>
+                            <td>R.C.F.Nº</td>
+                            <td>Hrs.Administ.Acad.</td>
+                            <td>R.C.F.Nº</td>
+                            <td>TOTAL HORAS AUTORIZADAS</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>TIEMPO PARCIAL</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>DEDICACION EXCLUSIVA</td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+
+                <td rowspan="2" width="10%"><h6>FACULTAD</h6></td>
 
                 <ul>
                     <?php
@@ -105,6 +255,25 @@
 
                     <?php } ?>
                 </ul>
+                <ul>
+                    <?php
+                    foreach ($arregloMateria as $Materia) { ?>
+                        <li> <?= $materia['NOMBRE_DOC'] ?></li>
+                        <li> <?= $materia['APELLPA_DOC'] ?></li>
+
+                    <?php } ?>
+                </ul>
+
+
+                <ul>
+                    <?php
+                    foreach ($arregloHorario as $Horario) { ?>
+                        <li> <?= $Horario['NOMBRE_DOC'] ?></li>
+                        <li> <?= $horario['APELLPA_DOC'] ?></li>
+
+                    <?php } ?>
+                </ul>
+
             </div>
         </div>
     </div>
